9842e8ab6e3389dc511371d61d77e600a2913397
Christopher Redden <christopher.redden@gmail.com>
2022-01-10 18:35:43 +0100

    Initial commit of base system

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const FLYINGCIRCUS = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ FLYINGCIRCUS.abilities = {
+  "str": "FLYINGCIRCUS.AbilityStr",
+  "dex": "FLYINGCIRCUS.AbilityDex",
+  "con": "FLYINGCIRCUS.AbilityCon",
+  "int": "FLYINGCIRCUS.AbilityInt",
+  "wis": "FLYINGCIRCUS.AbilityWis",
+  "cha": "FLYINGCIRCUS.AbilityCha"
+};
+
+FLYINGCIRCUS.abilityAbbreviations = {
+  "str": "FLYINGCIRCUS.AbilityStrAbbr",
+  "dex": "FLYINGCIRCUS.AbilityDexAbbr",
+  "con": "FLYINGCIRCUS.AbilityConAbbr",
+  "int": "FLYINGCIRCUS.AbilityIntAbbr",
+  "wis": "FLYINGCIRCUS.AbilityWisAbbr",
+  "cha": "FLYINGCIRCUS.AbilityChaAbbr"
+};
